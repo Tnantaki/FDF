73745999faa72d51782229923bdaf10335736e0b
Tanin Nantakitgosol <tnantaki@z2t17c1.42bangkok.com>
2023-04-14 21:06:54 +0700

    by alias

--- mandatory/my_mlx.c
+++ mandatory/my_mlx.c
@@ -31,28 +31,26 @@ void	my_mlx_clear_image(t_param *par)
 	mlx_put_image_to_window(par->mlx, par->win, par->img.ptr, 0, 0);
 }
 
-void	calculate_node(t_param *par, t_node **node, int change)
+void	calculate_node(t_param *par, t_node **node)
 {
-	if (change == 1)// zooom
-	{
-		move_node(node, par);
-		par->move_x = 0;
-		par->move_y = 0;
-	}
-	else if (change == 2)// zooom
-	{
-		init_node(par->node, par);
-		project_isometric_node(par->node, par);
-		render_line(&par->img, par->node, par);
-	}
+	init_node(node, par);
+	project_isometric_node(node, par);
+	set_node_center(node, par);
+	move_node(node, par);
+	render_line(&par->img, node, par);
 	my_mlx_clear_image(par);
 	render_line(&par->img, node, par);
 	mlx_put_image_to_window(par->mlx, par->win, par->img.ptr, 0, 0);
 }
 
-int	move_model(int keycode, t_param *par)
+int	key_hook(int keycode, t_param *par)
 {
-	if (keycode == KEY_W || keycode == KEY_UP)
+	int	event;
+	
+	event = 1;
+	if (keycode == KEY_ESC)
+		close_win(par);
+	else if (keycode == KEY_W || keycode == KEY_UP)
 		par->move_y -= MOVE_RATIO;
 	else if (keycode == KEY_A || keycode == KEY_LEFT)
 		par->move_x -= MOVE_RATIO;
@@ -60,35 +58,13 @@ int	move_model(int keycode, t_param *par)
 		par->move_y += MOVE_RATIO;
 	else if (keycode == KEY_D || keycode == KEY_RIGHT)
 		par->move_x += MOVE_RATIO;
-	else
-		return (0);
-	return (1);
-}
-
-int	zoom_model(int keycode, t_param *par)
-{
-	if (keycode == KEY_PLUS)
+	else if (keycode == KEY_PLUS)
 		par->span *= ZOOM_RATIO;
 	else if (keycode == KEY_MINUS)
 		par->span /= ZOOM_RATIO;
 	else
 		return (0);
-	return (1);
-}
-
-int	key_hook(int keycode, t_param *par)
-{
-	if (keycode == KEY_ESC)
-		close_win(par);
-	if (move_model(keycode, par) == 1)
-	{
-		calculate_node(par, par->node, 1);
-		return (0);
-	}
-	else if (zoom_model(keycode, par) == 1)
-	{
-		calculate_node(par, par->node, 2);
-		return (0);
-	}
+	if (event)
+		calculate_node(par, par->node);
 	return (0);
 }
